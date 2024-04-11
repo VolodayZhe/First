a0dfdfaf13c4512de91e46fda545e241d8e6975c
VolodayZhe <1997likeaboss@gmail.com>
2024-04-11 22:32:11 +0300

    1

--- pythonProjectAny/main.py
+++ pythonProjectAny/main.py
@@ -1,6 +1,6 @@
 import logging
 from telegram.ext import Application, MessageHandler, filters
-from config import BOT_TOKEN
+from configyy import BOT_TOKEN
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
